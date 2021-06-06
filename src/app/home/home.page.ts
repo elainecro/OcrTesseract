adc8e7a8394f2c11987b6f310ec64f95f37a989b
Elaine Oliveira <elaine.oliveira@itixti.com.br>
2021-06-06 13:34:34 -0300

    Iniciando teste de reconhecimento de texto com tesseract

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { createWorker} from 'tesseract.js';
+import '@ionic/pwa-elements';
+import { Camera, CameraResultType, CameraSource  } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,55 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  worker: Tesseract.Worker;
+  workerReady = false;
+  //image = "https://tesseract.projectnaptha.com/img/eng_bw.png";
+  //image = "assets/imgsOCR/1.png";
+  image = "assets/imgsOCR/5.jpeg";
+  ocrResult = '';
+  captureProgress = 0;
 
-  constructor() {}
+  constructor() {
+    this.loadWorker();
+  }
+
+  async loadWorker() {
+    this.worker = createWorker({
+      logger: progress => {
+        console.log(progress);
+        if (progress.status == 'recognizing text') {
+          this.captureProgress = parseInt('' + progress.progress * 100);
+        }
+      }
+    });
+
+    await this.worker.load();
+    // await this.worker.loadLanguage('por');    
+    // await this.worker.initialize('por');
+    await this.worker.loadLanguage('eng');
+    await this.worker.initialize('eng');
+    console.log('LOAD WORKER');
+
+    this.workerReady = true;
+  }
+
+  async captureImage() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    console.log('image: ', image);
+    this.image = image.dataUrl;
+  }
+
+  async recognizeImage() {
+    const result = await this.worker.recognize(this.image);
+    console.log(result);
+
+    this.ocrResult = result.data.text;
+  }
 
 }
